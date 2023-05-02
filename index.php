43921ef193f47fa6d03f771e000a31f4d2b5df22
Audrey <audrey.dedieu@beecoming.fr>
2023-05-02 15:38:47 +0200

    modif mail

--- index.php
+++ index.php
@@ -148,10 +148,10 @@
         echo '';
         echo implode("<br>", $fichier); // Affichage du tableau Array avec un passage à la ligne.
         
-        mail("rosa_lisa@orange.fr", "Le sujet", "Le message", "rosa_lisa@orange.fr");
+        mail("mailexpediteur@orange.fr", "Le sujet", "Le message", "mailenreceveur@orange.fr");
         ?>
 
-        <form method="post" action="">
+        <!-- <form method="post" action="">
             <label for="destinataire">Destinataire</label><br>
             <input type="text" name="destinataire" id="destinataire" placeholder="destinataire"><br><br>
             <label for="expediteur">Expediteur</label><br>
@@ -164,21 +164,21 @@
         </form>
 
         <?php
-        if (!empty($_POST)) {
-            // affichage des saisies pour être sur de les obtenir avant de les exploiter.
-            echo "destinataire : $_POST[destinataire] <br>";
-            echo "sujet : $_POST[sujet] <br>";
-            echo "message : $_POST[message] <br>";
-            echo "expediteur : $_POST[expediteur] <br>";
-            // entête email
-            $headers = 'MIME-Version: 1.0' . "\n";
-            $headers .= 'Content-type: text/html; charset=ISO-8859-1' . "\n";
-            $headers .= 'Reply-To: ' . $_POST['expediteur'] . "\n";
-            $headers .= 'From: "' . ucfirst(substr($_POST['expediteur'], 0, strpos($_POST['expediteur'], '@'))) . '"<' . $_POST['expediteur'] . '>' . "\n";
-            $headers .= 'Delivered-to: ' . $_POST['destinataire'] . "\n";
-            mail($_POST['destinataire'], $_POST['sujet'], $_POST['message'], $headers);
-        }
-        ?>
+        // if (!empty($_POST)) {
+        //     // affichage des saisies pour être sur de les obtenir avant de les exploiter.
+        //     echo "destinataire : $_POST[destinataire] <br>";
+        //     echo "sujet : $_POST[sujet] <br>";
+        //     echo "message : $_POST[message] <br>";
+        //     echo "expediteur : $_POST[expediteur] <br>";
+        //     // entête email
+        //     $headers = 'MIME-Version: 1.0' . "\n";
+        //     $headers .= 'Content-type: text/html; charset=ISO-8859-1' . "\n";
+        //     $headers .= 'Reply-To: ' . $_POST['expediteur'] . "\n";
+        //     $headers .= 'From: "' . ucfirst(substr($_POST['expediteur'], 0, strpos($_POST['expediteur'], '@'))) . '"<' . $_POST['expediteur'] . '>' . "\n";
+        //     $headers .= 'Delivered-to: ' . $_POST['destinataire'] . "\n";
+        //     mail($_POST['destinataire'], $_POST['sujet'], $_POST['message'], $headers);
+        // }
+        ?> -->
     </div>
 
 
